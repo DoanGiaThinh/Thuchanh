374e7757961bda43b3ef4f5e3b8e4c27b94ee058
DoanGiaThinh <bonemqst@gmail.com>
2024-10-17 10:29:43 +0700

    version 2

--- sever.js
+++ sever.js
@@ -1,34 +1,17 @@
-import express from 'express'
-import dontenv from 'dotenv/config'
-import myDateTime from './date'
-import {getPath,getParamsURL} from './getURL.js'
-import viewEngine from './viewEngine.js'
-const app = express()
-const port = process.env.PORT
-viewEngine(app)
-app.get('/', (req, res) =>{
-    // res.send('Hello World!')
-    res.render("home")
-})
-app.get('/about', (req, res) =>{
-    // res.send('Hello World!. Page about')
-    res.render("about")
-})
-app.get('/date', (req, res) =>{
-    const myDate = myDateTime()
-    res.send(`${myDate}`)
-})
-app.get('/geturl', (req, res) =>{
-    const url = getPath(req)
-    res.send(url)
-})
-app.get('/getparams', (req, res) =>{
-    const paramsurl = getParamsURL(req)
-    res.send(paramsurl)
-})
-app.get('/ejs',(req, res) =>{
-    res.render("test")
-})
-app.listen(port, () =>{
-    console.log(`Example app listening on port ${port}`)
-})
+import express from 'express';
+import dontenv from 'dotenv/config';
+import viewEngine from './viewEngine.js';
+import initWebRouter from './router/webRoute.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+viewEngine(app);
+
+
+initWebRouter(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
